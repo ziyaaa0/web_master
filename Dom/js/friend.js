4c1a798800edc631fa868ac3d1381e1e005961ad
pc <pc@email.com>
2024-11-19 17:51:20 +0900

    1119

--- Dom/js/friend.js
+++ Dom/js/friend.js
@@ -19,11 +19,23 @@ function makeRow(friendInfo = {
     document.querySelector('select[name = "friendBloodtype').value = this.children[4].innerHTML;
   })
 
-   let td = document.createElement('td');
-   let btn = document.createElement('input');
-   btn. setAttribute('type', 'checkbox');
-   td.appendChild(btn);
-   tr.appendChild(td);
+  let td = document.createElement('td');
+  let btn = document.createElement('input');
+  btn.setAttribute('type', 'checkbox');
+  btn.setAttribute('type', 'checkbox');
+  btn.addEventListener('click', (e) => e.stopPropagation());
+  btn.addEventListener('change', (e) => {
+    document.querySelector('thead input[type="checkbox"]').checked = true;
+    document.querySelectorAll('tbody input[type="checkbox"]').forEach(item => {
+      if(!item.checked) {
+      document.querySelector('thead input[type="checkbox"]').checked = false;
+    }
+  })
+
+  });
+
+  td.appendChild(btn);
+  tr.appendChild(td);
 
   for (let prop in friendInfo) {
     let td = document.createElement('td');
@@ -37,8 +49,10 @@ function makeRow(friendInfo = {
   btn.innerHTML = '삭제';
   btn.setAttribute('class', 'btn btn-danger');
   btn.addEventListener('click', (e) => {
+    e.stopPropagation(); //상위요소 이벤틑 차단.
     e.target.parentElement.parentElement.remove();
-  })
+
+  }) //3번째 매개값의 의미: bubbling, capturing 선택.
   td.appendChild(btn);
   tr.appendChild(td);
 
@@ -106,18 +120,47 @@ document.querySelector('button:nth-of-type(2)')
     }
   })
 
-  document.querySelector('button.btn.btn-danger')
+document.querySelector('button.btn.btn-danger')
   .addEventListener('click', (e) => {
     document.querySelectorAll('#list tr')
-    .forEach((item) => {
-      if(item.children[0].children[0].checked){
-        item.remove();
-      }
-    });
+      .forEach((item) => {
+        if (item.children[0].children[0].checked) {
+          item.remove();
+        }
+      });
   });
 
-  //전체선택하는 change 이벤트.
+//전체선택하는 change 이벤트.
+document.querySelector('thead input[type="checkbox"]')
+  .addEventListener('change', (e) => {
+    console.log(e.target.checked) //checkbox의 checked 속성.
+    //대상변경 : tbody의 체크박스
 
+    document.querySelectorAll('tbody input[type="checkbox"]')
+      .forEach((item) => {
+        item.checked = e.target.checked;
+      });
+  })
+
+
+
+//정보 전송 버튼 클릭시 친구의 정보를 localStorage에 저장.
+document.querySelector("button.btn-info")
+.addEventListener('click', (e) => {
+  let ary = [];
+  document.querySelectorAll('#list tr').forEach(item => {
+    console.log(item);
+    let name = item.children[1].innerHTML;
+    let phone = item.children[2].innerHTML;
+    let birth = item.children[3].innerHTML;
+    let blood = item.children[4].innerHTML;
+    let obj = {name, phone, birth, blood}
+    ary.push(obj);
+  });
+  console.log(ary);
+  let json = JSON.stringify(ary);
+  localStorage.setItem('friendList', json);
+})
 
 
 
